e95c2fac630e63dc1c83536c4d9295b856cab8d2
Schlueter <bs@bschlueter.com>
2015-01-27 10:35:08 -0500

    Simplify ReversePolishInput some more

--- src/app/ReversePolishInput.directive.js
+++ src/app/ReversePolishInput.directive.js
@@ -1,14 +1,8 @@
 'use strict';
 
-var REVERSE_POLISH_CHARACTERS = /^[0-9+* \-\/]*$/,
-    NUMERALS = /^[0-9]*$/,
+var NUMERALS = /^[\d]*$/,
     OPERATORS = /^[+*\-\/]*$/,
-    /**
-     * This dirctive uses explicit operation functions to avoid using
-     * eval; * in this case eval would almost certainly be safe because
-     * the inputs are checked, but this isn't particularly difficult
-     * or confusing, so it will be used.
-     */
+    // This dirctive uses explicit operation functions to avoid using eval
     OPERATIONS = {
         '+': function(operand1, operand2) { return operand1 + operand2 },
         '-': function(operand1, operand2) { return operand1 - operand2 },
@@ -18,66 +12,66 @@ var REVERSE_POLISH_CHARACTERS = /^[0-9+* \-\/]*$/,
 
 angular.module('reversePolish')
     .directive('reversePolishInput', function() {
-        function link(scope, elm, attrs, ctrl) {
 
-            function calculate(value) {
+        function simpleOperation(operand1, operand2, operator) {
+            if (NUMERALS.test(operand1) &&
+                NUMERALS.test(operand2) &&
+                OPERATORS.test(operator)) {
 
-                function countRegexMatches(regex, array) {
-                    return array.filter(function (element) {
-                        return regex.test(element);
-                    }).length;
-                }
-
-                var tokens = value.split(' '),
-                    numeralCount = countRegexMatches(NUMERALS, tokens),
-                    operatorCount = countRegexMatches(OPERATORS, tokens),
-                    result;
-
-                /**
-                 * Ensure the first two tokens are numerals and that
-                 * there are the correct number of operators
-                 * compared to operands for the input to be valid
-                 */
-                if (!(NUMERALS.test(tokens[0]) &&
-                      NUMERALS.test(tokens[1]) &&
-                      numeralCount === operatorCount + 1)){
-                    return;
-                }
-
-                function simpleOperation(operand1, operand2, operator) {
-                    if (NUMERALS.test(operand1) &&
-                        NUMERALS.test(operand2) &&
-                        OPERATORS.test(operator)) {
-
-                        operand1 = Number(operand1);
-                        operand2 = Number(operand2);
-                        operator = OPERATIONS[operator];
-                    } else {
-                        return;
-                    }
-
-                    return operator(operand1, operand2);
-                }
-
-                if (tokens && OPERATORS.test(tokens[2])) {
-                    result = simpleOperation(Number(tokens[0]),
-                                             Number(tokens[1]),
-                                             tokens[2]);
-                }
-
-                scope.result = result;
-
-                return result;
+                operand1 = Number(operand1);
+                operand2 = Number(operand2);
+                operator = OPERATIONS[operator];
+            } else {
+                return;
             }
 
+            return operator(operand1, operand2);
+        }
+
+        function countRegexMatches(regex, array) {
+            return array.filter(function (element) {
+                return regex.test(element);
+            }).length;
+        }
+
+        function calculate(value) {
+
+            var tokens = value.split(' '),
+                numeralCount = countRegexMatches(NUMERALS, tokens),
+                operatorCount = countRegexMatches(OPERATORS, tokens),
+                result;
+
+            /**
+             * Ensure the first two tokens are numerals and that
+             * there are the correct number of operators
+             * compared to operands for the input to be valid;
+             * otherwise return undefined
+             */
+            if (!(NUMERALS.test(tokens[0]) &&
+                  NUMERALS.test(tokens[1]) &&
+                  numeralCount === operatorCount + 1)){
+                return;
+            }
+
+            if (tokens && OPERATORS.test(tokens[2])) {
+                result = simpleOperation(Number(tokens[0]), Number(tokens[1]), tokens[2]);
+            }
+
+            return result;
+        }
+
+        function link(scope, elm, attrs, ctrl) {
+
             ctrl.$validators.reversePolish = function(modelValue, viewValue) {
+
                 if (ctrl.$isEmpty(modelValue)) {
                     // consider empty models to be valid
                     return true;
                 }
 
-                if (REVERSE_POLISH_CHARACTERS.test(viewValue) &&
-                    calculate(viewValue)) {
+                // Calculate result and apply to scope
+                scope.result = calculate(viewValue);
+                if (scope.result) {
                     // it is valid
                     return true;
                 }
@@ -87,6 +81,7 @@ angular.module('reversePolish')
                 return false;
             };
         }
+
         return {
             restrict: 'A',
             require: 'ngModel',
