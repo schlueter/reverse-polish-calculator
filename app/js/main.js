dfadfff8637f78b2de73bc6c9f7a4aae42670a30
Schlueter <bs@bschlueter.com>
2015-01-27 00:20:41 -0500

    Create initial calculator controller and directive

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-angular.module('myApp', []);
+angular.module('reverse-polish-calculator', [])
+    .controller('main', function($scope) {
+        $scope.result = 'result';
+    })
+    .directive('calculator', function () {
+        function link() {
+
+        }
+
+        return {
+            restrict: 'E',
+            link: link,
+            templateUrl: 'partials/calculator.html'
+        };
+    });
