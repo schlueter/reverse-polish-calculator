f7911e5770416e8f23f970024f94e424f81676de
Schlueter <bs@bschlueter.com>
2015-01-27 03:05:07 -0500

    Create reversePolishInput directive to validate input

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,10 @@
 'use strict';
 
+var REVERSE_POLISH_CHARACTERS = /^[0-9+* \-\/]*$/
+
 angular.module('reversePolishCalculator', [])
     .controller('reversePolishCalculatorCtrl', function($scope) {
-        $scope.entry = ''
+        $scope.entry = '';
         $scope.result = 'result';
     })
     .directive('reversePolishCalculator', function () {
@@ -30,5 +32,27 @@ angular.module('reversePolishCalculator', [])
             restrict: 'E',
             link: link,
             templateUrl: 'partials/calculator.html'
+        };
+    })
+    .directive('reversePolishInput', function() {
+        function link(scope, elm, attrs, ctrl) {
+            ctrl.$validators.reversePolish = function(modelValue, viewValue) {
+                if (ctrl.$isEmpty(modelValue)) {
+                  // consider empty models to be valid
+                  return true;
+                }
+
+                if (REVERSE_POLISH_CHARACTERS.test(viewValue)) {
+                  // it is valid
+                  return true;
+                }
+
+                // it is invalid
+                return false;
+            };
+        }
+        return {
+            require: 'ngModel',
+            link: link
         };
     });
