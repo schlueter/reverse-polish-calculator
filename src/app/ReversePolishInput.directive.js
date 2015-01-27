94b3d173984ed9689f7917846d1ca73939394394
Schlueter <bs@bschlueter.com>
2015-01-27 04:10:09 -0500

    Add counting function to test parseablity

--- src/app/ReversePolishInput.directive.js
+++ src/app/ReversePolishInput.directive.js
@@ -1,9 +1,28 @@
 'use strict';
 
-var REVERSE_POLISH_CHARACTERS = /^[0-9+* \-\/]*$/
+var REVERSE_POLISH_CHARACTERS = /^[0-9+* \-\/]*$/,
+    NUMERALS = /^[0-9]*$/,
+    OPERATORS = /^[+* \-\/]*$/;
+
 
 angular.module('reversePolishCalculator')
     .directive('reversePolishInput', function() {
+        function parsable(value) {
+            function countRegexMatches(regex) {
+                return function(count, nextValue) {
+                    return count + (regex.test(nextValue) ? 1 : 0);
+                }
+            }
+
+            var tokens = value.split(' '),
+                numeralCount = tokens.reduce(countRegexMatches(NUMERALS), 0),
+                operatorCount = tokens.reduce(countRegexMatches(OPERATORS), 0)
+
+            return NUMERALS.test(tokens[0]) &&
+                   NUMERALS.test(tokens[1]) &&
+                   numeralCount === operatorCount + 1;
+        }
+
         function link(scope, elm, attrs, ctrl) {
             ctrl.$validators.reversePolish = function(modelValue, viewValue) {
                 if (ctrl.$isEmpty(modelValue)) {
@@ -11,7 +30,7 @@ angular.module('reversePolishCalculator')
                   return true;
                 }
 
-                if (REVERSE_POLISH_CHARACTERS.test(viewValue)) {
+                if (REVERSE_POLISH_CHARACTERS.test(viewValue) && parsable(viewValue)) {
                   // it is valid
                   return true;
                 }
